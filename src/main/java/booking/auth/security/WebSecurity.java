b94824ce2fd3ef84eef87a7a2bfd1a8e2e4ace37
kamioteppei <kamioteppei@gmail.com>
2019-02-25 16:46:29 +0900

    modify WebSecurity.java

--- src/main/java/booking/auth/security/WebSecurity.java
+++ src/main/java/booking/auth/security/WebSecurity.java
@@ -13,6 +13,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.context.annotation.Bean;
 
+import static booking.auth.security.SecurityConstants.GET_BOOKABLE_URL;
 import static booking.auth.security.SecurityConstants.SIGN_UP_URL;
 
 @EnableWebSecurity
@@ -29,6 +30,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+                .antMatchers(HttpMethod.GET, GET_BOOKABLE_URL).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
